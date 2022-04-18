5ea16e66572cee835690dce6eb4d2b32abf4c686
Evgeny <sev.20@uni-dubna.ru>
2022-04-18 22:05:33 +0300

    [03] Fix typo

--- 03_truth_table_for_expr/main.py
+++ 03_truth_table_for_expr/main.py
@@ -4,7 +4,7 @@ while True:
     inp = input("Type expression: ")
     try:
         expr = Expression(inp)
-        print("Prefix notation:", expr.expr)
+        print("Postfix notation:", expr.expr)
         break
     except ExpressionParseError as e:
         print("Parse error:", e)
