291e61de00eb97e4280f3c4dadda916bf2528bed
Evgeny <sev.20@uni-dubna.ru>
2022-05-17 16:16:47 +0300

    [01] Return infinity as count of operators for empty expression

--- 01_normal_forms/utils.py
+++ 01_normal_forms/utils.py
@@ -2,4 +2,8 @@ BINARY_OPERATORS = ['*', 'V', '^']
 
 
 def count_binary_operators(expr):
-    return len(list(filter(lambda x: x in BINARY_OPERATORS, list(expr))))
+    count = len(list(filter(lambda x: x in BINARY_OPERATORS, list(expr))))
+    if count <= 0:
+        count = float('inf')
+
+    return count
